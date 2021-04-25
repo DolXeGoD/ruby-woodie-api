db9355ffb28de31ebe61846670df6ac1d8b6aa4f
HyunJae-Jo <dolxegod@gmail.com>
2021-04-25 15:32:52 +0900

    first commit

---
+++ src/main/java/com/ruby_woodie/maybetodo/MaybetodoApplication.java
@@ -0,0 +1,13 @@
+package com.ruby_woodie.maybetodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaybetodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaybetodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ruby_woodie/maybetodo/MaybetodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruby_woodie.maybetodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaybetodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
