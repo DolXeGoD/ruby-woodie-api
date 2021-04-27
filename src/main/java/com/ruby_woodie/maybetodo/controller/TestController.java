dd7034efd83ea79032d623b2b7bd39a7d9b43cdd
조현재 <dolxegod@gmail.com>
2021-04-28 00:51:12 +0900

    RestController

--- src/main/java/com/ruby_woodie/maybetodo/controller/TestController.java
+++ src/main/java/com/ruby_woodie/maybetodo/controller/TestController.java
@@ -4,23 +4,21 @@ import com.ruby_woodie.maybetodo.domain.entity.TestEntity;
 import com.ruby_woodie.maybetodo.service.TestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
-@RequestMapping(value = "/test")
+@RestController
+@RequestMapping(value = "api/test")
 @RequiredArgsConstructor
 public class TestController {
     private final TestService testService;
 
-    @GetMapping
-    public String hello(Model model) {
-        List<TestEntity> testList = testService.getTestList();
-        model.addAttribute("testList", testList);
-        return "list";
+    @GetMapping(value = "/all")
+    public List<TestEntity> testAll() {
+        return testService.getTestList();
     }
 
 }
